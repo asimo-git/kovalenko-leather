28b584df9c84f0b3cd127610ecefd69ced50bc83
Asimo <rvolenc@gmail.com>
2025-06-02 20:11:44 +0400

    feat: change about section

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -2,30 +2,29 @@ import Image from "next/image";
 
 export default function About() {
   return (
-    <>
-      <section className="flex my-25 justify-center items-center">
-        <div className="w-[30vw] h-[30vw] overflow-hidden">
-          <Image
-            src="/foto.jpg"
-            alt="фото мастера"
-            width={500}
-            height={500}
-            className="w-full h-full object-cover"
-          ></Image>
-        </div>
-        <div className="max-w-[800px] p-8 text-2xl">
-          Здравствуйте! <br></br>
-          <br></br> Меня зовут Павел Коваленко, и я рад приветствовать вас в
-          моей мастерской, где каждая строчка и каждая складка — результат
-          внимательной работы рук и многолетнего опыта. <br></br> <br></br> Моя
-          мастерская — это место, где кожа превращается в изделие. Это
-          пространство, где восстанавливается уют, обновляется комфорт и
-          рождаются индивидуальные вещи с душой. Я люблю кожу за её характер.
-          Она требует уважения, внимания и терпения. Именно это я вкладываю в
-          каждую свою работу. Моя цель — не просто починить или заменить, а
-          сделать так, чтобы вы захотели показать это другим.
-        </div>
-      </section>
-    </>
+    <section className="section md:flex-row my-12 gap-8">
+      <div className="w-full min-w-80 max-w-[300px] md:max-w-[30vw] aspect-square overflow-hidden shadow-md">
+        <Image
+          src="/foto.jpg"
+          alt="фото мастера"
+          width={500}
+          height={500}
+          className="w-full h-full  object-cover"
+        />
+      </div>
+
+      <div className="max-w-2xl text-base md:text-xl leading-relaxed">
+        <p>
+          <strong>Здравствуйте!</strong>
+          <br />
+          <br />
+          Меня зовут Павел, и вот уже более 10 лет я помогаю оживить любимую
+          мебель, обновить интерьер автомобиля и создать уют с помощью авторских
+          подушек. Работаю в нескольких городах, выезжаю на замеры и
+          консультации, подбираю материалы под интерьер и бюджет, а каждую
+          работу выполняю как для себя — аккуратно, качественно и с душой.
+        </p>
+      </div>
+    </section>
   );
 }
